937b050658bbf3ded7813a54ac582daac243bf9a
allriseyy <allriseyy@gmail.com>
2025-06-07 02:02:19 +0100

    more sample

--- music.py
+++ music.py
@@ -1,32 +1,86 @@
-from mido import Message, MidiFile, MidiTrack, bpm2tempo
 
-mid = MidiFile()
-track = MidiTrack()
-mid.tracks.append(track)
+import pretty_midi
 
-tempo = bpm2tempo(80)
-track.append(Message('program_change', program=0, time=0))
+# Create a PrettyMIDI object
+midi = pretty_midi.PrettyMIDI()
 
-# Define chord progression (Am, F, C, G)
+# Instruments (GarageBand-compatible General MIDI numbers)
+# Acoustic Grand Piano = 0
+# Acoustic Guitar (nylon) = 24
+# Electric Bass (finger) = 33
+# Pad 2 (warm) = 89
+
+# Create instruments
+piano = pretty_midi.Instrument(program=0)       # Soft chords
+guitar = pretty_midi.Instrument(program=24)     # Main dreamy rhythm
+bass = pretty_midi.Instrument(program=33)       # Soulful bass
+pad = pretty_midi.Instrument(program=89)        # Ambient background
+
+# Define tempo
+bpm = 75
+qn = 60.0 / bpm  # quarter note duration
+
+# Chord progression: C - Am - F - G (standard romantic)
 chords = [
-    [57, 60, 64],  # A minor
-    [53, 57, 60],  # F major
-    [48, 52, 55],  # C major
-    [55, 59, 62]   # G major
+    ['C4', 'E4', 'G4'],
+    ['A3', 'C4', 'E4'],
+    ['F3', 'A3', 'C4'],
+    ['G3', 'B3', 'D4']
 ]
 
-# Add chords
-for _ in range(2):  # repeat progression twice
-    for chord_notes in chords:
-        for note in chord_notes:
-            track.append(Message('note_on', note=note, velocity=70, time=0))
-        for note in chord_notes:
-            track.append(Message('note_off', note=note, velocity=70, time=480))
+melody = [
+    ['E4', 'G4', 'C5', 'E5'],   # In my eyes
+    ['C4', 'E4', 'A4', 'C5'],   # she's a beautiful girl
+    ['A3', 'C4', 'F4', 'A4'],   # with a heavenly smile
+    ['B3', 'D4', 'G4', 'B4'],   # got me falling for her
+    ['E4', 'F4', 'G4', 'E4'],   # every day
+    ['C4', 'D4', 'E4', 'G4'],   # now my world is complete
+    ['A3', 'B3', 'C4', 'E4'],   # just you and me
+    ['G3', 'A3', 'B3', 'D4'],   # you will be all that I need
+]
 
-# Add lead melody (C5–D5–E5…)
-melody = [72, 74, 76, 74, 72, 71, 69, 71, 72, 74, 76, 77, 76, 74, 72, 71]
-for note in melody:
-    track.append(Message('note_on', note=note, velocity=90, time=120))
-    track.append(Message('note_off', note=note, velocity=90, time=120))
+# Function to add notes
+def add_notes(inst, notes, start, duration, velocity):
+    for pitch in notes:
+        note_num = pretty_midi.note_name_to_number(pitch)
+        note = pretty_midi.Note(velocity=velocity, pitch=note_num,
+                                start=start, end=start + duration)
+        inst.notes.append(note)
+        start += duration
+    return start
 
-mid.save("dreamy_indie_rnb_python.mid")
+# Add chords and pad
+time = 0.0
+for i, chord_notes in enumerate(chords * 2):  # 8 bars total
+    for note_name in chord_notes:
+        note_num = pretty_midi.note_name_to_number(note_name)
+        ch_note = pretty_midi.Note(velocity=50, pitch=note_num,
+                                   start=time, end=time + 4 * qn)
+        piano.notes.append(ch_note)
+        pad.notes.append(ch_note)
+    time += 4 * qn
+
+# Add bass line
+time = 0.0
+bass_roots = ['C2', 'A1', 'F1', 'G1'] * 2
+for root in bass_roots:
+    note_num = pretty_midi.note_name_to_number(root)
+    bass_note = pretty_midi.Note(velocity=70, pitch=note_num,
+                                 start=time, end=time + 4 * qn)
+    bass.notes.append(bass_note)
+    time += 4 * qn
+
+# Add lead melody
+time = 0.0
+for phrase in melody:
+    time = add_notes(guitar, phrase, time, qn, velocity=85)
+
+# Add all instruments to MIDI
+midi.instruments.append(piano)
+midi.instruments.append(guitar)
+midi.instruments.append(bass)
+midi.instruments.append(pad)
+
+# Save the MIDI file
+midi.write("yungkai_dreamy_rnb_chorus.mid")
+print("MIDI file saved as yungkai_dreamy_rnb_chorus.mid")
